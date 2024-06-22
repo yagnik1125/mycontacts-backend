7f9d3f9fe80ad01d16427de403eb3e90af0678be
yagnik1125 <yagnikvasoya1125@gmail.com>
2024-06-22 18:12:13 +0530

    contact

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -3,9 +3,10 @@ const router = express.Router();
 const { getContacts, createContact, getContact, updateContact, deleteContact } = require("../controllers/contactController"); //import controller
 const validateToken = require("../middleware/validateTokenHandler");
 
-router.use(validateToken);
+router.use(validateToken);//all request need token verification so we can do it globally
 
 router.route("/").get(getContacts);
+// router.route("/").get(validateToken,getContacts);//another way if we want to verify token for any perticular request
 
 router.route("/").post(createContact);
 
