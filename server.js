f2a379354d92c4d89c5791d1d12a32228cb53324
yagnik1125 <yagnikvasoya1125@gmail.com>
2026-05-23 12:19:36 +0530

    remove network listen

--- server.js
+++ server.js
@@ -29,7 +29,3 @@ app.use(errorHandler);
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
-
-app.listen(port, '0.0.0.0', () => {
-  console.log(`Server running on port ${port}`);
-});
